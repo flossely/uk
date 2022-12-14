e3c4d88682cdcd41e96cb282dcb941eae53f2f2c
Karl August Nestor <jesuscaesar@outlook.com>
2022-12-14 04:40:58 +0000

    Improved functionality

--- uk.metro.php
+++ uk.metro.php
@@ -8,6 +8,7 @@ $metro['uk'] =
         "bshp" => "Body",
         "fshp" => "Feet",
         "birth" => "Birth",
+        "zodiac" => "Zodiac",
         "country" => "Country",
         "ratio" => "Ratio",
         "height" => "Height",
