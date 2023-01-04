838a1545721aa7a9659a9cc31cee93c2ade1fc01
Karl August Nestor <jesuscaesar@outlook.com>
2023-01-04 07:33:59 +0000

    Added new wallpapers

--- uk.parle.php
+++ uk.parle.php
@@ -133,6 +133,7 @@ $parle['uk'] =
     "term:era" => "Era",
     "term:era:pl" => "Eras",
     "term:fallback" => "Fallback Font",
+    "term:monospace" => "Monospace Font",
     "term:file" => "File",
     "term:filename" => "Filename",
     "term:font" => "Font",
